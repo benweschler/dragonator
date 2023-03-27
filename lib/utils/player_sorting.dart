0554b5695785b67c40246854f5f1b5c162790fae
Ben Weschler <benjaminweschler@gmail.com>
2023-03-26 22:31:24 -0700

    Made alpha sorting case-blind.

--- lib/utils/player_sorting.dart
+++ lib/utils/player_sorting.dart
@@ -2,16 +2,21 @@ import 'package:dragonator/data/player.dart';
 
 abstract class PlayerSort {
   static const sortingStrategyLabels = {
-    'First Name' : firstName,
-    'Last Name' : lastName,
-    'Weight' : weight,
+    'First Name': firstName,
+    'Last Name': lastName,
+    'Weight': weight,
   };
 
   static int firstName(Player a, Player b) =>
-      a.firstName.compareTo(b.firstName);
+      a.firstName.caseBlindCompare(b.firstName);
 
   static int lastName(Player a, Player b) =>
-      a.lastName.compareTo(b.lastName);
+      a.lastName.caseBlindCompare(b.lastName);
 
   static int weight(Player a, Player b) => a.weight.compareTo(b.weight);
+}
+
+extension _StringComparison on String {
+  int caseBlindCompare(String other) =>
+      toLowerCase().compareTo(other.toLowerCase());
 }
