19d4102631102e8cfa1b437818baac7ed0020101
Ben Weschler <benjaminweschler@gmail.com>
2024-11-27 05:15:24 -0800

    Add software keyboard avoidance to popups.

--- lib/widgets/popup_dialog.dart
+++ lib/widgets/popup_dialog.dart
@@ -9,27 +9,41 @@ class PopupDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ConstrainedBox(
-        constraints: BoxConstraints(
-          maxWidth: MediaQuery.of(context).size.width * 0.85,
-        ),
-        child: Material(
-          color: Theme.of(context).colorScheme.surfaceContainerLow,
-          borderRadius: Corners.medBorderRadius,
-          elevation: 15,
-          child: Padding(
-            padding: const EdgeInsets.all(Insets.lg),
-            child: child,
-          ),
-        ).animate().slideY(
-              // The the default duration of the transition animation in
-              // showDialog
-              duration: 200.ms,
-              begin: 0.06,
-              end: 0,
-              curve: Curves.easeOut,
+    return AnimatedPadding(
+      // Avoid the onscreen keyboard.
+      padding: MediaQuery.viewInsetsOf(context),
+      // The default duration and curve for the build-in Dialog widget.
+      duration: Duration(milliseconds: 100),
+      curve: Curves.decelerate,
+      child: MediaQuery.removeViewInsets(
+        removeLeft: true,
+        removeTop: true,
+        removeRight: true,
+        removeBottom: true,
+        context: context,
+        child: Center(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: MediaQuery.of(context).size.width * 0.85,
             ),
+            child: Material(
+              color: Theme.of(context).colorScheme.surfaceContainerLow,
+              borderRadius: Corners.medBorderRadius,
+              elevation: 15,
+              child: Padding(
+                padding: const EdgeInsets.all(Insets.lg),
+                child: child,
+              ),
+            ).animate().slideY(
+                  // The the default duration of the transition animation in
+                  // showDialog.
+                  duration: 200.ms,
+                  begin: 0.06,
+                  end: 0,
+                  curve: Curves.easeOut,
+                ),
+          ),
+        ),
       ),
     );
   }
