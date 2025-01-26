5153a2724de3080c0a3799015737c7f01f156858
Ben Weschler <benjaminweschler@gmail.com>
2025-01-26 01:55:39 -0800

    Updated Android module.

--- android/app/src/main/kotlin/com/benweschler/dragonator/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.benweschler.dragonator
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/dev/benweschler/dragonator/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.benweschler.dragonator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
